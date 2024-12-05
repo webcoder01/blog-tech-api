8861f020d6bf3df53c06eb7deca9446a4c5c04ae
Antoine Raymond <antoine.raymond@teksial.com>
2024-12-05 15:12:03 +0100

    feat(post): add field description

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -413,6 +413,7 @@ export interface ApiPostPost extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::post.post'> &
       Schema.Attribute.Private;
